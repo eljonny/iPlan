db4d3f668e55b695726aaeb29f520b80d3231e51
Jonathan Hyry <jonathan.hyry@outlook.com>
2011-04-27 05:24:21 +0000

    git-svn-id: https://subversion.assembla.com/svn/cs446@3 b385dc55-2d91-4f6a-9923-a7e5823291d4

---
+++ GUIProj1/Window1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUIProj1
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        private gridObject[,] dataGrid;
+        public Window1()
+        {
+            InitializeComponent();
+            dataGrid = new gridObject[48,7];
+            for(int i = 0;i<dataGrid.GetLength(0);i++)
+            {
+                for(int j = 0;j<dataGrid.GetLength(1);j++)
+                {
+
+                }
+            }
+
+        }
+        public gridObject[,] setArray(int x,int y)
+        {
+            dataGrid = new gridObject[x,y];
+            return dataGrid;
+        }
+
+        private void UniformGrid_SourceUpdated(object sender,DataTransferEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ GUIProj1/gridObject.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GUIProj1
+{
+    class gridObject
+    {
+        private int row, col;
+        private string timeBeg, timeEnd, gObjCont;
+        private bool isPopulated;
+
+        public gridObject()
+        {
+            row = 0;
+            col = 0;
+            timeBeg = "";
+            timeEnd = "";
+            isPopulated = false;
+        }
+        
+        public gridObject(int r,int c,string tb,string te,bool pop)
+        {
+            row = r;
+            col = c;
+            timeBeg = tb;
+            timeEnd = te;
+            isPopulated = pop;
+        }
+
+        protected void setCoords(int r,int c)
+        {
+            row = r;
+            col = c;
+        }
+
+        protected void setTimes(string b, string e)
+        {
+            timeBeg = b;
+            timeEnd = e;
+        }
+        
+        private void checkPop()
+        {
+            if(timeBeg=="" || timeEnd=="")
+                isPopulated = false;
+            else
+                isPopulated = true;
+        }
+
+        protected int[] getRC()
+        {
+            int[] rc = { row,col };
+            return rc;
+        }
+
+        protected string[] getTimeVals()
+        {
+            string[] begEndTimes = { timeBeg,timeEnd };
+            return begEndTimes;
+        }
+
+        public bool isPop()
+        {
+            this.checkPop();
+            return isPopulated;
+        }
+    }
+}
