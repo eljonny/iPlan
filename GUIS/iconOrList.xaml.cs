82bcb7b7a74f033adff90b344773ca94de6ef667
Jonathan Hyry <jonathan.hyry@outlook.com>
2011-06-01 00:46:00 +0000

    git-svn-id: https://subversion.assembla.com/svn/cs446@28 b385dc55-2d91-4f6a-9923-a7e5823291d4

---
+++ GUIS/iconOrList.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace GUIProj1
+{
+    /// <summary>
+    /// Interaction logic for iconOrList.xaml
+    /// </summary>
+    public partial class iconOrList : NavigationWindow
+    {
+        public iconOrList()
+        {
+            InitializeComponent();
+            checkedList navPage = new checkedList();
+
+        }
+    }
+}
